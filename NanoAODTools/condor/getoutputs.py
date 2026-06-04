d383995f13be4f925c9a32fc2d6bf0d7d97af339
Andrea <andrea.camilla.puglia@cern.ch>
2026-06-04 12:44:47 +0200

    update main

--- NanoAODTools/condor/getoutputs.py
+++ NanoAODTools/condor/getoutputs.py
@@ -12,7 +12,7 @@ from checkjobs import get_file_sizes, find_folder, job_exit_code, checkSubmitSta
 usage = 'python3 getoutputs.py -d dataset_name'
 parser = optparse.OptionParser(usage)
 parser.add_option('-d', '--dat', dest='dat', type=str, default = '', help='Please enter a dataset name')
-parser.add_option('-o', '--output', dest='output', type=str, default = '../python/postprocessing/samples/dict_samples_2022.json', help='Please enter a json output file')
+parser.add_option('-o', '--output', dest='output', type=str, default = '../python/postprocessing/samples/dict_samples_trainings_2024.json', help='Please enter a json output file')
 parser.add_option('--tier', dest='tier', type=str, default = 'bari', help='Please enter location where to write the output file (tier pisa or bari)')
 
 (opt, args) = parser.parse_args()
@@ -41,7 +41,7 @@ os.popen("cp /tmp/x509up_u" + str(uid) + " /afs/cern.ch/user/" + inituser + "/" 
 
 # insert here the name of output folder
 running_folder                      = os.environ.get('PWD') + "/tmp/post_processing/"
-remote_folder_name                  = "TROTA2022/Training_samples"
+remote_folder_name                  = "TROTA2024/Training_samples"
 
 def get_files_on_tier(folder, cert_path, ca_path):
     try:
